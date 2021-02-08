fd260a82970f0fb03140617c43470263a2b2943c
luca-bottero <lucabottero99@gmail.com>
2021-02-09 00:00:12 +0100

    Update bdt_eff_bdt_out.png, model_out_GT_0.0.png, and 87 more files...

--- training.py
+++ training.py
@@ -9,7 +9,7 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from hipe4ml.model_handler import ModelHandler
 from hipe4ml.tree_handler import TreeHandler
-from hipe4ml.analysis_utils import train_test_generator
+from hipe4ml.analysis_utils import train_test_generator, bdt_efficiency_array
 from hipe4ml import plot_utils
 #%%
 print('Loading Monte-Carlo data...')
@@ -41,7 +41,7 @@ print(len(mc_signal.get_data_frame()))          #2272026
 # TRAINING WITH UNBALANCED DATASET
 
 train_test_data = train_test_generator([mc_signal, backgorund_ls], [1,0], test_size=0.5)
-
+  
 #%%
 
 model_clf = xgb.XGBClassifier()
@@ -63,21 +63,25 @@ roc_train_test_fig = plot_utils.plot_roc_train_test(train_test_data[3], y_pred_t
                                                     train_test_data[1], y_pred_train, None, leg_labels)
 
 #%%
+plt.hist(data.get_data_frame()['model_output'],bins=100)
+
+
+#%%
 
 data.apply_model_handler(model_hdl)
-selected_data_hndl = data.get_subset('model_output > 0.95')
+selected_data_hndl = data.get_subset('model_output > 0.95 & m>=2.96 & m<=3.04')
 
 labels_list = ["after selection","before selection"]
 colors_list = ['orangered', 'cornflowerblue']
-plot_utils.plot_distr([selected_data_hndl, data], column='pt', bins=100, labels=labels_list, colors=colors_list, density=True,fill=True,  alpha=0.5)    #histtype='step',
+plot_utils.plot_distr([selected_data_hndl, data], column='pt', bins=34, labels=labels_list, colors=colors_list, density=True,fill=True,  alpha=0.5)    #histtype='step',
 ax = plt.gca()
 ax.set_xlabel(r'$p_T$ (GeV/$c$)')
 ax.margins(x=0)
 ax.xaxis.set_label_coords(0.9, -0.075)
 
-labels_list = ["after selection","before selection"]
-colors_list = ['orangered', 'cornflowerblue']
-plot_utils.plot_distr([selected_data_hndl, data], column='m', bins=200, labels=labels_list, colors=colors_list, density=True,fill=True,  alpha=0.5)
+labels_list = ["after selection"]
+colors_list = ['orangered']
+plot_utils.plot_distr([selected_data_hndl, data], column='m', bins=34, labels=labels_list, colors=colors_list, density=True,fill=True,  alpha=0.5)
 ax = plt.gca()
 ax.set_xlabel(r'Mass (GeV/$c^2$)')
 ax.margins(x=0)
@@ -85,7 +89,7 @@ ax.xaxis.set_label_coords(0.9, -0.075)
 
 labels_list = ["after selection","before selection"]
 colors_list = ['orangered', 'cornflowerblue']
-plot_utils.plot_distr([selected_data_hndl, data], column='centrality',log=True, bins=100, labels=labels_list, colors=colors_list, density=True,fill=True,  alpha=0.5)
+plot_utils.plot_distr([selected_data_hndl, data], column='centrality',log=True, bins=34, labels=labels_list, colors=colors_list, density=True,fill=True,  alpha=0.5)
 ax = plt.gca()
 ax.set_xlabel(r'Mass (GeV/$c^2$)')
 ax.margins(x=0)
@@ -95,7 +99,33 @@ print(len(selected_data_hndl))
 print(len(selected_data_hndl)/len(data))
 
 # %%
+max_score = max(data.get_data_frame()['model_output'])
+
+for score in np.arange(0., max_score, 0.1):
+    selected_data_hndl = data.get_subset('m >= 2.96 & m<= 3.04 & model_output > ' + str(score))
+    plot_utils.plot_distr(selected_data_hndl,labels='score > ' + str(np.round(score,2)), column='m', bins=34, colors='orange', density=True,fill=True)
+    ax = plt.gca()
+    ax.set_xlabel(r'Mass (GeV/$c^2$)')
+    ax.margins(x=0)
+    ax.xaxis.set_label_coords(0.9, -0.075)
+    plt.savefig('./images/model_out_GT_' + str(score) + '.png',dpi = 300, facecolor = 'white')
 
 
+
+# %%
+backgorund_ls.apply_model_handler(model_hdl)
+mc_signal.apply_model_handler(model_hdl)
+mc_bkg_merge = train_test_generator([mc_signal, backgorund_ls], [1,0], test_size=0.99)
+bdt_efficiency = bdt_efficiency_array(mc_bkg_merge[3],mc_bkg_merge[2]['model_output'])
+
+plt.plot(bdt_efficiency[1],bdt_efficiency[0])
+plt.title("BDT efficiency as a function of BDT output")
+plt.xlabel('BDT output')
+plt.ylabel('Efficiency')
+plt.savefig('./images/bdt_eff_bdt_out.png',dpi=300,facecolor='white')
+
+
+
+# %%
 
 # %%
