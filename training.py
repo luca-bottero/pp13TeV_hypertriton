4a5cb60d7abe3b32052f7fc16383934d00a48d1a
luca-bottero <lucabottero99@gmail.com>
2021-02-09 17:31:40 +0100

    Corrected error for efficiency plot, use hipe4ml to plot feature importance

--- training.py
+++ training.py
@@ -35,9 +35,6 @@ print(len(data.get_data_frame()))               #104732
 print(len(backgorund_ls.get_data_frame()))      #103200
 print(len(mc_signal.get_data_frame()))          #2272026
 
-#The dataset would be highly unbalanced, thus I think it would be better to sample the MC datas to make a 
-#balanced dataset. I will try both options
-
 # %%
 # TRAINING WITH UNBALANCED DATASET
 
@@ -59,14 +56,10 @@ plt.rcParams["figure.figsize"] = (10, 7)
 leg_labels = ['background', 'signal']
 
 ml_out_fig = plot_utils.plot_output_train_test(model_hdl, train_test_data, 100, 
-                                               False, leg_labels, True, density=True)
+                                               True, leg_labels, True, density=True)
 
 roc_train_test_fig = plot_utils.plot_roc_train_test(train_test_data[3], y_pred_test,
                                                     train_test_data[1], y_pred_train, None, leg_labels)
-
-#%%
-plt.hist(data.get_data_frame()['model_output'],bins=100)
-
 
 #%%
 
@@ -75,23 +68,15 @@ selected_data_hndl = data.get_subset('model_output > 0.95 & m>=2.96 & m<=3.04')
 
 labels_list = ["after selection","before selection"]
 colors_list = ['orangered', 'cornflowerblue']
-plot_utils.plot_distr([selected_data_hndl, data], column='pt', bins=34, labels=labels_list, colors=colors_list, density=True,fill=True,  alpha=0.5)    #histtype='step',
+plot_utils.plot_distr([selected_data_hndl, data], column='pt', bins=34, labels=labels_list, colors=colors_list,log=True, density=False,fill=True,  alpha=0.5)    #histtype='step',
 ax = plt.gca()
 ax.set_xlabel(r'$p_T$ (GeV/$c$)')
 ax.margins(x=0)
 ax.xaxis.set_label_coords(0.9, -0.075)
 
-labels_list = ["after selection"]
-colors_list = ['orangered']
-plot_utils.plot_distr([selected_data_hndl, data], column='m', bins=34, labels=labels_list, colors=colors_list, density=True,fill=True,  alpha=0.5)
-ax = plt.gca()
-ax.set_xlabel(r'Mass (GeV/$c^2$)')
-ax.margins(x=0)
-ax.xaxis.set_label_coords(0.9, -0.075)
-
 labels_list = ["after selection","before selection"]
 colors_list = ['orangered', 'cornflowerblue']
-plot_utils.plot_distr([selected_data_hndl, data], column='centrality',log=True, bins=34, labels=labels_list, colors=colors_list, density=True,fill=True,  alpha=0.5)
+plot_utils.plot_distr([selected_data_hndl, data], column='centrality',log=True, bins=34, labels=labels_list, colors=colors_list, density=False,fill=True,  alpha=0.5)
 ax = plt.gca()
 ax.set_xlabel(r'Mass (GeV/$c^2$)')
 ax.margins(x=0)
@@ -103,20 +88,19 @@ print(len(selected_data_hndl)/len(data))
 # %%
 max_score = max(data.get_data_frame()['model_output'])
 
-for score in np.arange(0., max_score, 0.1):
-    selected_data_hndl = data.get_subset('m >= 2.96 & m<= 3.04 & model_output > ' + str(score))
-    plot_utils.plot_distr(selected_data_hndl,labels='score > ' + str(np.round(score,2)), column='m', bins=34, colors='orange', density=True,fill=True)
-    ax = plt.gca()
-    ax.set_xlabel(r'Mass (GeV/$c^2$)')
-    ax.margins(x=0)
-    ax.xaxis.set_label_coords(0.9, -0.075)
-    plt.savefig('./images/model_out_GT_' + str(np.round(score,2)) + '.png',dpi = 300, facecolor = 'white')
+if False:
+    for score in np.arange(0., max_score, 0.1):
+        selected_data_hndl = data.get_subset('model_output > ' + str(score))
+        plot_utils.plot_distr(selected_data_hndl,labels='score > ' + str(np.round(score,2)), column='m', bins=34, colors='orange', density=False,fill=True, range=[2.96,3.04])
+        ax = plt.gca()
+        ax.set_xlabel(r'Mass (GeV/$c^2$)')
+        ax.margins(x=0)
+        ax.xaxis.set_label_coords(0.9, -0.075)
+        plt.savefig('./images/model_out_GT_' + str(np.round(score,2)) + '.png',dpi = 300, facecolor = 'white')
 
 # %%
-backgorund_ls.apply_model_handler(model_hdl)
-mc_signal.apply_model_handler(model_hdl)
-mc_bkg_merge = train_test_generator([mc_signal, backgorund_ls], [1,0], test_size=0.99)
-bdt_efficiency = bdt_efficiency_array(mc_bkg_merge[3],mc_bkg_merge[2]['model_output'])
+# BDT EFFICIENCY AS FUNCTION OF BDT OUTPUT
+bdt_efficiency = bdt_efficiency_array(train_test_data[3],y_pred_test)
 
 plt.plot(bdt_efficiency[1],bdt_efficiency[0])
 plt.title("BDT efficiency as a function of BDT output")
@@ -124,12 +108,23 @@ plt.xlabel('BDT output')
 plt.ylabel('Efficiency')
 plt.savefig('./images/bdt_eff_bdt_out.png',dpi=300,facecolor='white')
 
+#%%
+# BDT FEATURE IMPORTANCE USING HIPE4ML WITH SHAP
+
+feat_imp_1, feat_imp_2 = plot_utils.plot_feature_imp(train_test_data[2],train_test_data[3],model_hdl,approximate=False)
+feat_imp_1.savefig('./images/feature_importance_HIPE4ML_violin.png',dpi=300,facecolor='white')
+feat_imp_2.savefig('./images/feature_importance_HIPE4ML_bar.png',dpi=300,facecolor='white')
+
 # %%
+# BDT FEATURE IMPORTANCE USING XGBOOST METHOD
 xgb_model = model_hdl.get_original_model()
 sorted_idx = xgb_model.feature_importances_.argsort()
 plt.barh([training_variables[i] for i in sorted_idx], xgb_model.feature_importances_[sorted_idx])
 plt.xlabel("Xgboost Feature Importance")
 plt.savefig('./images/xgb_feature_importance.png', dpi=300, facecolor='white')
 
+
+# %%
+train_test_data[2]
 
 # %%
