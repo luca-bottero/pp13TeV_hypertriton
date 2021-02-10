33cb0fc1337f8da3f1ede1416bc79e1cdcdb8e81
luca-bottero <lucabottero99@gmail.com>
2021-02-10 10:30:24 +0100

    Update mass_fit.cpython-38.pyc, bdt_eff_bdt_out.png, and 53 more files...

--- mass_fit.py
+++ mass_fit.py
@@ -10,18 +10,37 @@ import aghast
 import ROOT
 from hipe4ml.model_handler import ModelHandler
 from hipe4ml import plot_utils
+from array import array
 
 def mass_fitter(hist,score):
     aghast_hist = aghast.from_numpy(hist)
     root_hist = aghast.to_root(aghast_hist,'Score_' + str(np.round(score,4)))
+
     canvas = ROOT.TCanvas()
     root_hist.Draw()
     canvas.Draw()
-    gaus = ROOT.TF1('gaus','gaus',2.985,3.005)
-    bkg = ROOT.TF1('linear','pol 2',2.96,3.04)
-    total = ROOT.TF1('total','gaus + [3]',2.96,3.04)
-    root_hist.Fit('gaus','R')
-    root_hist.Fit('linear','R+')
+
+    gaus = ROOT.TF1('gaus','gaus',2.96,3.04)
+    bkg = ROOT.TF1('poly','pol 2',2.96,3.04)
+    total = ROOT.TF1('total','gaus + pol 2',2.96,3.04)
+    
+    gaus.SetLineColor( 1 )
+    bkg.SetLineColor( 2 )
+    total.SetLineColor( 3 )
+
+    root_hist.Fit('gaus','R','',2.985,3.005)
+    root_hist.Fit('poly','R+','',2.96,3.04)
+
+    par = array( 'd', 6*[0.] )
+    gaus_par = gaus.GetParameters()
+    poly_par = bkg.GetParameters()
+    par[0], par[1], par[2] = gaus_par[0], gaus_par[1], gaus_par[2]
+    par[3], par[4], par[5] = poly_par[0], poly_par[1], poly_par[2]
+
+    total.SetParameters(par)
+    root_hist.Fit('total', 'R+','',2.96,3.04 )
+
+    ROOT.gStyle.SetOptFit(1111)
     canvas.SaveAs('./images/mass_sys/score_' + str(np.round(score,4)) + '.png')
 
 
