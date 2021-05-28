Add sidebands normalization, fix plot order

src/mass_fit.py
@@ -14,7 +14,23 @@ from hipe4ml.model_handler import ModelHandler
 from hipe4ml import plot_utils
 from array import array
 
+def normalize_ls(data_counts, ls_counts, bins):
+    bin_centers = 0.5 * (bins[1:] + bins[:-1])
+    side_region = np.logical_or(bin_centers<2.992-2*0.0025, bin_centers>2.992+2*0.0025)
+    
+    side_data_counts = np.sum(data_counts[side_region])
+    side_ls_counts = np.sum(ls_counts[side_region])
+    scaling_factor = side_data_counts/side_ls_counts
 
+    return scaling_factor
+
+def h1_invmass(counts, mass_range=[2.96, 3.04] , bins=34, name=''):
+    th1 = ROOT.TH1D(f'{name}',f'{name}', int(bins), mass_range[0], mass_range[1])
+    for index in range(0, len(counts)):
+        th1.SetBinContent(index+1, counts[index])
+        # th1.SetBinError(index + 1, np.sqrt(counts[index]))
+    th1.SetDirectory(0)
+    return th1
 
 def mass_fitter(hist,score,efficiency):
     aghast_hist = aghast.from_numpy(hist)
@@ -61,10 +77,12 @@ def mass_fitter(hist,score,efficiency):
     return count, error
 
 
-def data_ls_comp_plots(data, ls, scores, efficiencies, filename_dict):
+def data_ls_comp_plots(data, ls, scores, efficiencies, filename_dict, flag_dict):
     ROOT.gROOT.LoadMacro("cmb_fit_exp.C")
     ROOT.gROOT.LoadMacro("cmb_fit_erf.C")
     from ROOT import cmb_fit_exp, cmb_fit_erf
+
+    n_bins = 80     #PARAM !!!
 
     ff = ROOT.TFile(filename_dict['analysis_path'] + 'results/data_ls.root','recreate')
     
@@ -72,19 +90,39 @@ def data_ls_comp_plots(data, ls, scores, efficiencies, filename_dict):
         selected_data = data.query('model_output > ' + str(score))
         selected_ls = ls.query('model_output > ' + str(score))
 
-        hist_data = np.histogram(selected_data['m'],bins=80,range=(2.96,3.04))
-        hist_ls = np.histogram(selected_ls['m'],bins=80,range=(2.96,3.04))
+        hist_data = np.histogram(selected_data['m'],bins=n_bins,range=(2.96,3.04))
 
-        aghast_hist = aghast.from_numpy(hist_data)
+        if flag_dict['norm_on_sidebands']:
+            hist_ls_TMP = np.histogram(selected_ls['m'],bins=n_bins,range=(2.96,3.04))
+            ls_counts = np.array(hist_ls_TMP[0]) * normalize_ls(hist_data[0], hist_ls_TMP[0], hist_data[1])
+            #hist_ls = np.histogram(list(np.round(ls_counts)) ,bins=n_bins,range=(2.96,3.04))
+        else:
+            hist_ls = np.histogram(selected_ls['m'],bins=n_bins,range=(2.96,3.04))
+            ls_counts = np.array(hist_ls[0])
+
+        ''' if flag_dict['norm_on_sidebands']:
+            hist_ls = list(hist_ls)
+            hist_ls[0] = np.array(hist_ls[0])*normalize_ls(hist_data[0], hist_ls[0], hist_data[1])
+            #hist_ls = tuple(hist_ls)'''
+
+        '''aghast_hist = aghast.from_numpy(hist_data)
         root_hist_data = aghast.to_root(aghast_hist,'Efficiency ' + str(np.round(efficiency,4)))
         root_hist_data_erf = aghast.to_root(aghast_hist,'Efficiency_erf ' + str(np.round(efficiency,4)))
 
         aghast_hist = aghast.from_numpy(hist_ls)
         root_hist_ls = aghast.to_root(aghast_hist, 'Efficiency_ls ' +str(np.round(efficiency,4)))
         root_hist_ls_erf = aghast.to_root(aghast_hist, 'Efficiency_ls_erf ' +str(np.round(efficiency,4)))
+        root_hist_data.SetTitle('Counts as a function of mass;m [GeV/c^{2}];Counts')'''
+
+        root_hist_data = h1_invmass(hist_data[0], name = 'eff_' + str(np.round(efficiency,4)), bins = n_bins)
+        root_hist_data_erf = h1_invmass(hist_data[0], name = 'eff_erf_' + str(np.round(efficiency,4)), bins = n_bins)
+
+        root_hist_ls = h1_invmass(np.round(ls_counts), name = 'eff_' + str(np.round(efficiency,4)), bins = n_bins)
+        root_hist_ls_erf = h1_invmass(np.round(ls_counts), name = 'eff_erf_' + str(np.round(efficiency,4)), bins = n_bins)
+
         root_hist_data.SetTitle('Counts as a function of mass;m [GeV/c^{2}];Counts')
 
-        canvas = ROOT.TCanvas('Efficiency ' + str(np.round(efficiency,4)))
+        canvas = ROOT.TCanvas('eff_' + str(np.round(efficiency,4)))
 
         leg = ROOT.TLegend(.6,.8,.8,.9)
         leg.AddEntry(root_hist_data, 'Data', 'L')
@@ -96,10 +134,14 @@ def data_ls_comp_plots(data, ls, scores, efficiencies, filename_dict):
         root_hist_ls.SetMarkerStyle(7)
         root_hist_data.SetMarkerStyle(7)
 
-        if root_hist_data.Integral() > root_hist_ls.Integral():
+        if root_hist_data.GetMaximum() > root_hist_ls.GetMaximum():
+            root_hist_data.SetTitle('Counts as a function of mass;m [GeV/c^{2}];Counts')
+
             root_hist_data.Draw('PE')
             root_hist_ls.Draw('PE SAME')
         else:
+            root_hist_ls.SetTitle('Counts as a function of mass;m [GeV/c^{2}];Counts')
+
             root_hist_ls.Draw('PE')
             root_hist_data.Draw('PE SAME')
 
@@ -110,8 +152,8 @@ def data_ls_comp_plots(data, ls, scores, efficiencies, filename_dict):
 
         canvas.Write()
 
-        cmb_fit_exp(root_hist_ls,root_hist_data, 'Fit_exp_eff_' + str(np.round(efficiency,4)))
-        cmb_fit_erf(root_hist_ls_erf,root_hist_data_erf, 'Fit_erf_eff_' + str(np.round(efficiency,4)))
+        cmb_fit_exp(root_hist_ls,root_hist_data, 'fit_exp_eff_' + str(np.round(efficiency,4)))
+        cmb_fit_erf(root_hist_ls_erf,root_hist_data_erf, 'fit_erf_eff_' + str(np.round(efficiency,4)))
 
         #canvas.SaveAs('../analysis/images/mass_distr_LS/LS_hist_eff_' + str(np.round(efficiency,4)) + '.png')
 
@@ -132,7 +174,7 @@ def systematic_estimate(data,scores,efficiencies):
         errors.append(err)
         i += 1
 
-    plt.plot(efficiencies,count)
+    '''plt.plot(efficiencies,count)
     #plt.fill_between(efficiencies, np.array(count) - np.array(errors), np.array(count) + np.array(errors), alpha = 0.1)
     plt.title('Count of signal (from fit) as a function of efficiency')
     plt.xlabel('Efficiency')
@@ -148,6 +190,6 @@ def systematic_estimate(data,scores,efficiencies):
     plt.xlabel('Efficiency')
     plt.ylabel('Count')
     plt.annotate('Mean: ' + str(np.round(np.mean(count),4)) + "\n$\sigma$: " + str(np.round(np.std(count),4)) ,xy=(0.68,23))
-    plt.savefig('../analysis/images/mass_distr/fit_count_eff_rect.png',dpi=300,facecolor = 'white')
+    plt.savefig('../analysis/images/mass_distr/fit_count_eff_rect.png',dpi=300,facecolor = 'white')'''
 
 #%%