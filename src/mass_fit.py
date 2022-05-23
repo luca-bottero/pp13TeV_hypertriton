fdf9f44589d5194daf3b0db3e4281ed376b39df7
Luca Bottero <lucabottero99@gmail.com>
2022-05-23 17:56:10 +0200

    Added significance calculation

--- src/mass_fit.py
+++ src/mass_fit.py
@@ -34,46 +34,70 @@ def h1_invmass(counts, mass_range=[2.96, 3.04] , bins=34, name=''):
     th1.SetDirectory(0)
     return th1
 
-def mass_fitter(hist,score,efficiency, filename_dict):
+def mass_fitter(hist, score, efficiency, filename_dict):
     aghast_hist = aghast.from_numpy(hist)
     root_hist = aghast.to_root(aghast_hist,'Efficiency ' + str(np.round(efficiency,4)))
+
+    ff = ROOT.TFile(filename_dict['analysis_path'] + 'results/data_ls_integral.root','update')
 
     #root_hist = h1_invmass(hist[0], name = 'eff_' + str(np.round(efficiency,4)), bins = 34)
 
     root_hist.SetTitle('Counts as a function of mass;m [GeV/c^{2}];Counts')
 
-    canvas = ROOT.TCanvas()
+    canvas = ROOT.TCanvas(name = 'eff_' + str(np.round(efficiency,4)))
     root_hist.Draw('PE')
     canvas.Draw('PE SAME')
 
     gaus = ROOT.TF1('gaus','gaus',2.96,3.04)
-    bkg = ROOT.TF1('poly','pol 1',2.96,3.04)
-    total = ROOT.TF1('total','pol1(0) + gaus(2)',2.96,3.04)
-    total.SetParameter(3, 2.992)
+    bkg = ROOT.TF1('sigmoid','[0]/(1 + TMath::Exp(-[1]*(x-[2]))) + [3]',2.96,3.04)
+    bkg.SetParLimits(3, 0., 5.)
+    bkg.SetParameter(0, 15)
+    bkg.SetParLimits(2, 2.99, 3.0)
+    bkg.SetParLimits(1, 10., 500.)
+    #bkg.SetParLimits(3, 2.)
+
+    total = ROOT.TF1('total','gaus(0) + [3]/(1 + TMath::Exp(-[4]*(x-[5]))) + [6]',2.96,3.04)
+
+    '''total.SetParameter(3, 2.992)
     total.SetParLimits(3, 2.99, 2.995)
     total.SetParameter(2, 100)  #26
     total.SetParameter(4, 0.0032)
-    total.SetParLimits(4, 0.001, 0.01)
+    total.SetParLimits(4, 0.001, 0.01)'''
+    '''total.SetParameter(1, 2.992)
+    total.SetParLimits(1, 2.99, 2.995)
+    total.SetParameter(0, 100)  #26
+    total.SetParameter(2, 0.0032)
+    total.SetParLimits(2, 0.001, 0.01)'''
     
-    gaus.SetLineColor( 1 )
+    gaus.SetLineColor( 4 )
     bkg.SetLineColor( 2 )
     total.SetLineColor( 3 )
+
+    gaus.SetLineStyle(7)
+    bkg.SetLineStyle(7)
 
     m = 2.9912
     d_m = 3 * 0.002
 
-    root_hist.Fit('gaus','R','',m - d_m, m + d_m)
-    root_hist.Fit('poly','R+','',2.96,3.04)
+    root_hist.Fit('gaus','R0','',m - d_m, m + d_m)
+    root_hist.Fit('sigmoid','R0+','',2.96,3.04)
+
+    #gaus.SetNpx(500)
+    gaus.Draw('SAME')
+
+    #bkg.SetNpx(500)
+    bkg.Draw('SAME')
     
-    par = array( 'd', 6*[0.] )
+    par = array( 'd', 7*[0.] )
     gaus_par = gaus.GetParameters()
-    poly_par = bkg.GetParameters()
+    bkg_par = bkg.GetParameters()
     par[0], par[1], par[2] = gaus_par[0], gaus_par[1], gaus_par[2]
-    par[3], par[4], par[5] = poly_par[0], poly_par[1], poly_par[2]
+    par[3], par[4], par[5], par[6] = bkg_par[0], bkg_par[1], bkg_par[2], bkg_par[3]
     
     s = gaus.Integral(m - d_m, m + d_m) * 100
     b = bkg.Integral(m - d_m, m + d_m) * 100
     significance = s/np.sqrt(s+b)
+    #significance = s/np.sqrt(b)
 
     print('Significance at', efficiency, 'efficiency')
     print('Signal', s)
@@ -82,14 +106,18 @@ def mass_fitter(hist,score,efficiency, filename_dict):
     print('Significance', significance)
     print('Significance x efficiency', efficiency * significance)
 
-
     total.SetParameters(par)
 
     total.SetNpx(1000)
-    root_hist.Fit('total', 'R+', '',2.96,3.04)
+    #root_hist.Fit('total', 'R+', '',2.96,3.04)
+    total.Draw('SAME')
+    root_hist.SetMarkerStyle(7)
 
     ROOT.gStyle.SetOptFit(1111)
     #ROOT.gStyle.SetOptFit(0)
+
+    canvas.Write()
+
     canvas.SaveAs(filename_dict['analysis_path'] + 'images/mass_distr_LS/LS_hist_eff_' + str(np.round(efficiency,4)) + '.png')
 
     '''count = total.GetParameters()[2]
@@ -97,6 +125,9 @@ def mass_fitter(hist,score,efficiency, filename_dict):
 
     count = root_hist.GetEntries()
     error = np.sqrt(count)
+
+
+    ff.Close()
 
     return count, error, significance
 
@@ -198,6 +229,10 @@ def systematic_estimate(data,scores,efficiencies, filename_dict):
     errors = []
     sigs = []
     i = 0
+
+    ff = ROOT.TFile(filename_dict['analysis_path'] + 'results/data_ls_integral.root','recreate')
+    ff.Close()
+
     for score in scores:
         selected_data_hndl = data.query('model_output > ' + str(score))
         #hist = plot_utils.plot_distr(selected_data_hndl, column='m', bins=34, colors='orange', density=False,fill=True, range=[2.96,3.04])
